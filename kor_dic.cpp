d142e8260819bc4048d09eb474fa8d958da95082
GyuPyo Shin <54663056+Gyupyo@users.noreply.github.com>
2020-08-13 23:07:19 +0900

    need to be considered

---
+++ kor_dic.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+int main()
+{
+	ifstream data_1, data_2;
+	ofstream out;
+
+	string data_1_str, data_2_str, data_ch, label = "AM";
+
+	data_1.open("animal_1.txt");
+	data_2.open("animal_2.txt");
+	out.open("out.txt");
+
+	if (!data_1.is_open())
+	{
+		cout << "Fail to open answer.txt" << endl;
+		return -1;
+	}
+
+	if (!data_2.is_open())
+	{
+		cout << "Fail to open data.txt" << endl;
+		return -1;
+	}
+
+	while (getline(data_1, data_1_str))
+	{
+		if (data_1_str != "#00")
+			continue;
+
+		getline(data_1, data_1_str);
+
+		data_1_str.erase(find(data_1_str.begin(), data_1_str.end(), '-'));
+		
+		out << data_1_str << " " << label << endl;
+	}
+
+	while (getline(data_2, data_2_str))
+	{
+		if (data_2_str != "#00")
+			continue;
+
+		getline(data_2, data_2_str);
+
+		data_2_str.erase(find(data_2_str.begin(), data_2_str.end(), '-'));
+
+		out << data_2_str << " " << label << endl;
+	}
+	cout << "DONE, CHECK out.txt" << endl;
+
+	data_1.close();
+	data_2.close();
+	out.close();
+
+	system("pause");
+	return 0;
+}
