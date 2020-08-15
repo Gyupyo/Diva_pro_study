096a5c75e318d022dee82a9cf4cb71c055266aa1
GyuPyo Shin <54663056+Gyupyo@users.noreply.github.com>
2020-08-15 15:20:50 +0900

    08/15

---
+++ code/erase_overlap.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////
+// File Name : compare.cpp										     //
+// Date: 2020/08/15												     //
+// Os: Windows 10												     //
+// Author: Shin Gyu Pyo						 					     //
+///////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	string DATA_FILE = "단문질문 QA dataset",
+		ANSWER_FILE = "세분류_수정";
+
+	ifstream data, answer;
+	ofstream out;
+
+	string data_str, answer_str, word, ner;
+	
+	vector<string> answer_vec;
+	vector<string>::iterator answer_it;
+	map<string, string> result;
+	map<string, string>::iterator result_it;
+	int dock;
+	
+	data.open(DATA_FILE + ".txt");
+	answer.open(ANSWER_FILE + ".txt");
+	out.open(DATA_FILE + "__LAP__OUT.txt");
+
+	while(!data.is_open())
+	{
+		cout << "Fail to open data.txt" << endl;
+		return -1;
+	}
+
+	while (!answer.is_open())
+	{
+		cout << "Fail to open answer.txt" << endl;
+		return -1;
+	}
+
+	while (getline(answer, answer_str))
+		answer_vec.push_back(answer_str);
+	
+	while (getline(data, data_str))
+	{
+		for (answer_it = answer_vec.begin(); answer_it != answer_vec.end(); answer_it++)
+			if ( data_str.find(*answer_it) != string::npos)
+			{
+				dock = data_str.find(*answer_it);
+				break;
+			}
+
+		word = data_str.substr(0, dock-1);
+		ner = data_str.substr(dock);
+		
+		result.insert(make_pair(word, ner));
+	}
+
+	for (result_it = result.begin(); result_it != result.end();result_it++)
+		out << result_it->first << " " << result_it->second << endl;
+
+	cout << "DONE, CHECK out.txt" << endl;
+
+	data.close();
+	out.close();
+	system("pause");
+	return 0;
+}
---
+++ code/find_ne.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <queue>
+#pragma warning(disable: 4996)
+
+///////////////////////////////////////////////////////////////////////
+// File Name : compare.cpp										     //
+// Date: 2020/08/15												     //
+// Os: Windows 10												     //
+// Author: Shin Gyu Pyo						 					     //
+///////////////////////////////////////////////////////////////////////
+using namespace std;
+
+int main()
+{
+	string file_name = "E2017.Wiki.001";
+
+	ifstream data;
+	ofstream out;
+
+	string data_str, text_str, type_str;
+	string::size_type morp, ne, find_end;
+
+	vector<string> text, type_a, type_b;
+	
+	vector<string>::iterator text_it, a_it, b_it;
+
+	bool flag = 0, match = 0;
+	
+	data.open(file_name+".txt");
+	out.open(file_name + "__NE__OUT.txt");
+
+	if (!data.is_open())
+	{
+		cout << "Fail to open data.txt" << endl;
+		return -1;
+	}
+
+	while (getline(data, data_str))
+	{
+		flag = 0;
+		while (getline(data, data_str))
+		{
+			morp = data_str.find("\"morp\"");
+
+			if (morp != string::npos)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (!flag)
+			break;
+
+		while (getline(data, data_str))
+		{
+			find_end = data_str.find("],");
+
+			if (find_end != string::npos)
+				break;
+	
+			// morp 시작
+			text_str = data_str.substr(data_str.find("\"lemma\"") + 10);
+			text_str = text_str.substr(0, text_str.find("\""));
+			
+			text.push_back(text_str);
+		}
+	
+		while (getline(data, data_str))
+		{
+			ne = data_str.find("\"NE\"");
+
+			if (ne != string::npos)
+				break;
+		}
+	
+		while (getline(data, data_str))
+		{
+			find_end = data_str.find("],");
+
+			if (find_end != string::npos)
+				break;
+
+			// type 시작
+			text_str = data_str.substr(data_str.find("\"text\"") + 9);
+			text_str = text_str.substr(0, text_str.find("\""));
+			type_a.push_back(text_str);
+		
+			type_str = data_str.substr(data_str.find("\"type\"") + 9);
+			type_str = type_str.substr(0, type_str.find("\""));
+			type_b.push_back(type_str);
+		}
+		
+			// 매칭 시작
+		for (text_it = text.begin(); text_it != text.end(); text_it++)
+		{
+			match = 0;
+			for (a_it = type_a.begin(), b_it = type_b.begin(); a_it != type_a.end(); a_it++, b_it++)
+				if (*text_it == *a_it)
+				{
+					match = 1;
+					break;
+				}
+
+			if (match)
+				out << *text_it << " " << *b_it << endl;
+			else
+				out << *text_it << " O" << endl;
+		}
+
+		out << endl;
+
+		text.clear();
+		system("pause");
+	}
+	cout << "DONE, CHECK out.txt" << endl;
+
+	data.close();
+	out.close();
+	system("pause");
+	return 0;
+}
